25975fbfe70b98c114c14ce3958d1b575f77089c
Xin Liu <sam@secondstate.io>
2022-03-04 07:16:27 +0000

    [Rust SDK][refactor](module, instance): Refactored the code.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge/src/instance/mod.rs
+++ bindings/rust/wasmedge/src/instance/mod.rs
@@ -1,7 +1,6 @@
 mod function;
 mod global;
 mod import;
-mod instance;
 mod memory;
 mod module;
 mod table;
@@ -9,7 +8,6 @@ mod table;
 pub use function::{Func, Signature, SignatureBuilder};
 pub use global::{Global, GlobalType};
 pub use import::{ImportMod, WasiImportMod, WasmEdgeProcessImportMod};
-pub use instance::Instance;
 pub use memory::{Memory, MemoryType};
-pub use module::{ExportType, ExternalType, ImportType, Module};
+pub use module::Instance;
 pub use table::{Table, TableType};
