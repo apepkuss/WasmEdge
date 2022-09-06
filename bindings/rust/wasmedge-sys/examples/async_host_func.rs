816a03bcbedd60e026d0c0c1f9cc881a8d9856c9
Xin Liu <sam@secondstate.io>
2022-09-06 16:46:08 +0800

    [Rust][refactor](examples): Used host function macros.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/examples/async_host_func.rs
+++ bindings/rust/wasmedge-sys/examples/async_host_func.rs
@@ -1,32 +1,33 @@
+use wasmedge_macro::sys_async_host_function;
 use wasmedge_sys::{CallingFrame, Executor, FuncType, Function, WasmValue};
 use wasmedge_types::error::HostFuncError;
 
+// Native async function
+#[sys_async_host_function]
+async fn say_hello(
+    _frame: &CallingFrame,
+    _args: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+    println!("Hello, world!");
+    Ok::<Vec<WasmValue>, HostFuncError>(vec![])
+}
+
+// // native async func wrapper
+// fn wrap_say_hello<'a>(
+//     frame: &'a CallingFrame,
+//     input: Vec<WasmValue>,
+// ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<Vec<WasmValue>, HostFuncError>> + 'a>>
+// {
+//     Box::pin(say_hello(&frame, input))
+// }
+
 #[cfg_attr(test, test)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Native async function
-    async fn say_hello(
-        _: &CallingFrame,
-        _: Vec<WasmValue>,
-    ) -> Result<Vec<WasmValue>, HostFuncError> {
-        println!("Hello, world!");
-        Ok::<Vec<WasmValue>, HostFuncError>(vec![])
-    }
-
-    // native async func wrapper
-    fn wrap_say_hello<'a>(
-        frame: &'a CallingFrame,
-        input: Vec<WasmValue>,
-    ) -> std::pin::Pin<
-        Box<dyn std::future::Future<Output = Result<Vec<WasmValue>, HostFuncError>> + 'a>,
-    > {
-        Box::pin(say_hello(&frame, input))
-    }
-
     // create a FuncType
     let func_ty = FuncType::create(vec![], vec![])?;
 
     // create a host function
-    let async_host_func = Function::create_async(&func_ty, Box::new(wrap_say_hello), 0)?;
+    let async_host_func = Function::create_async(&func_ty, Box::new(say_hello), 0)?;
 
     // run this function
     let mut executor = Executor::create(None, None)?;
--- bindings/rust/wasmedge-sys/examples/async_host_func2.rs
+++ bindings/rust/wasmedge-sys/examples/async_host_func2.rs
@@ -1,29 +1,40 @@
 use switcher2::stack::*;
+use wasmedge_macro::sys_async_host_function2;
 use wasmedge_sys::{
     async_env::AsyncWasmEdgeResult, CallingFrame, Executor, FuncType, Function,
     WasmEdgeHostFuncFuture, WasmEdgeHostFuncResult, WasmValue,
 };
+use wasmedge_types::error::HostFuncError;
+
+// Native async function
+#[sys_async_host_function2]
+async fn say_hello(
+    _frame: &CallingFrame,
+    _args: Vec<WasmValue>,
+) -> Result<Vec<WasmValue>, HostFuncError> {
+    println!("Hello, world!");
+    Ok::<Vec<WasmValue>, HostFuncError>(vec![])
+}
+
+// // Native async function
+// fn async_say_hello(frame: &CallingFrame, args: Vec<WasmValue>) -> WasmEdgeHostFuncFuture {
+//     let stack = EightMbStack::new().unwrap();
+//     AsyncWasmEdgeResult::<EightMbStack, WasmEdgeHostFuncResult<Vec<WasmValue>>, fn()>::new(
+//         stack,
+//         |mut yielder| -> Result<Vec<WasmValue>, HostFuncError> {
+//             yielder.async_suspend(say_hello(frame, args))
+//         },
+//     )
+//     .unwrap()
+// }
 
 #[cfg_attr(test, test)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Native async function
-    fn async_say_hello(_: &CallingFrame, _: Vec<WasmValue>) -> WasmEdgeHostFuncFuture {
-        let stack = EightMbStack::new().unwrap();
-        AsyncWasmEdgeResult::<EightMbStack, WasmEdgeHostFuncResult<Vec<WasmValue>>, fn()>::new(
-            stack,
-            |_| {
-                println!("Hello, world!");
-                Ok(vec![])
-            },
-        )
-        .unwrap()
-    }
-
     // create a FuncType
     let func_ty = FuncType::create(vec![], vec![])?;
 
     // create a host function
-    let async_host_func = Function::create_async_new(&func_ty, Box::new(async_say_hello), 0)?;
+    let async_host_func = Function::create_async_new(&func_ty, Box::new(say_hello), 0)?;
 
     // run this function
     let mut executor = Executor::create(None, None)?;
