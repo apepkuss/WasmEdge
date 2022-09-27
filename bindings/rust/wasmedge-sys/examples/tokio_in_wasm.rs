5ab592a9182f56f4f8fa3e9183c9ed2e93490328
Xin Liu <sam@secondstate.io>
2022-09-27 17:19:03 +0800

    [Rust][refactor](examples): Updated the example.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/examples/tokio_in_wasm.rs
+++ bindings/rust/wasmedge-sys/examples/tokio_in_wasm.rs
@@ -1,10 +1,16 @@
-use wasmedge_sys::Vm;
+use wasmedge_sys::{Config, Vm};
 
 #[cfg_attr(test, test)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm_file = std::path::Path::new("/Users/sam/workspace/rust/async-demo/run-tokio-in-wasm/target/wasm32-wasi/release/run_tokio_in_wasm.wasm");
 
-    let vm = Vm::create(None, None)?;
+    // create a Config context
+    let mut config = Config::create()?;
+    config.bulk_memory_operations(true);
+    config.wasi(true);
+    assert!(config.wasi_enabled());
+
+    let vm = Vm::create(Some(config), None)?;
     vm.run_wasm_from_file(wasm_file, "hello", [])?;
 
     Ok(())
