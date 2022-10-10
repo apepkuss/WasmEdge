cc45bb5de081510260fa91ac6c6e41e6191169b9
Xin Liu <sam@secondstate.io>
2022-10-10 11:04:46 +0800

    [Rust][refactor](examples): Refactored tokio_in_wasm example.
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-sys/examples/tokio_in_wasm.rs
+++ bindings/rust/wasmedge-sys/examples/tokio_in_wasm.rs
@@ -1,3 +1,11 @@
+//! Use the following command to run this example:
+//!
+//! ```bash
+//! cd bindings/rust
+//! cargo run -p wasmedge-sys --example tokio_in_wasm -- --nocapture
+//! ```
+//!
+
 use wasmedge_sys::{Config, Vm};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,7 +22,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // initialize the wasi module
     let mut wasi_module = vm.wasi_module_mut()?;
-    wasi_module.init_wasi(None, None, Some(vec![".:."]));
+    wasi_module.init_wasi(None, None, None);
 
     // run host function
     vm.run_wasm_from_file(wasm_file, "hello", [])?;
