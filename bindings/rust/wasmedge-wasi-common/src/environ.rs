278c7ac492e485a675d6277224edd65b158ec1b2
Xin Liu <sam@secondstate.io>
2023-01-05 22:15:19 +0800

    refactor(wasi-common): rename WasiCtx to Environ
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- bindings/rust/wasmedge-wasi-common/src/environ.rs
+++ bindings/rust/wasmedge-wasi-common/src/environ.rs
@@ -2,21 +2,26 @@ use crate::{
     dir::{DirCaps, DirEntry, WasiDir},
     error::WasiCommonError,
     file::{FileCaps, FileEntry, WasiFile},
+    pipe::{ReadPipe, WritePipe},
     FdMap, StringArray,
 };
 
-pub struct WasiCtx {
+pub struct Environ {
     pub args: StringArray,
     pub envs: StringArray,
     pub fd_map: FdMap,
 }
-impl WasiCtx {
+impl Environ {
     pub fn new() -> Self {
-        Self {
+        let mut environ = Self {
             args: StringArray::new(),
             envs: StringArray::new(),
             fd_map: FdMap::new(),
-        }
+        };
+        environ.set_stdin(Box::new(ReadPipe::new(std::io::empty())));
+        environ.set_stdout(Box::new(WritePipe::new(std::io::sink())));
+        environ.set_stderr(Box::new(WritePipe::new(std::io::sink())));
+        environ
     }
 
     pub fn push_arg(&mut self, arg: impl AsRef<str>) -> Result<(), WasiCommonError> {
@@ -54,33 +59,33 @@ impl WasiCtx {
         self.fd_map.push(Box::new(FileEntry::new(caps, file)))
     }
 
-    pub fn insert_file(
-        &mut self,
-        fd: i32,
-        file: Box<dyn WasiFile>,
-        caps: FileCaps,
-    ) -> Result<(), WasiCommonError> {
-        Ok(self
-            .fd_map
-            .insert_at(fd, Box::new(FileEntry::new(caps, file))))
+    pub fn insert_file(&mut self, fd: i32, file: Box<dyn WasiFile>, caps: FileCaps) {
+        self.fd_map
+            .insert_at(fd, Box::new(FileEntry::new(caps, file)));
     }
 
     pub fn set_stdin(&mut self, mut file: Box<dyn WasiFile>) {
-        unimplemented!()
+        let caps = Self::stdio_caps(&mut *file);
+        self.insert_file(0, file, caps);
     }
 
     pub fn set_stdout(&mut self, mut file: Box<dyn WasiFile>) {
-        unimplemented!()
+        let caps = Self::stdio_caps(&mut *file);
+        self.insert_file(1, file, caps);
     }
 
     pub fn set_stderr(&mut self, mut file: Box<dyn WasiFile>) {
-        unimplemented!()
+        let caps = Self::stdio_caps(&mut *file);
+        self.insert_file(2, file, caps);
     }
 
     fn stdio_caps(file: &mut dyn WasiFile) -> FileCaps {
         let mut caps = FileCaps::all();
 
-        if file.isatty() {
+        // If `file` is a tty, restrict the `tell` and `seek` capabilities, so
+        // that wasi-libc's `isatty` correctly detects the file descriptor
+        // as a tty.
+        if file.is_tty() {
             caps &= !(FileCaps::TELL | FileCaps::SEEK);
         }
 
--- bindings/rust/wasmedge-wasi-common/src/lib.rs
+++ bindings/rust/wasmedge-wasi-common/src/lib.rs
@@ -3,14 +3,15 @@
     html_favicon_url = "https://raw.githubusercontent.com/cncf/artwork/49169bdbc88a7ce3c4a722c641cc2d548bd5c340/projects/wasm-edge-runtime/icon/color/wasm-edge-runtime-icon-color.svg"
 )]
 
-pub mod ctx;
 pub mod dir;
+pub mod environ;
 pub mod error;
 pub mod fd_map;
 pub mod file;
+pub mod pipe;
 pub mod string_array;
 pub mod wasi_module;
 
-pub use ctx::WasiCtx;
+pub use environ::Environ;
 pub use fd_map::FdMap;
 pub use string_array::StringArray;
